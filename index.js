7de3cd85ac2a0a896a3dbc3ba534dc9ad8b41ab3
isaac stott <isaac.cloyd@gmail.com>
2020-06-01 15:31:56 -0600

    working on the assignment not sure if I am doing the error messages right.

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+let users = [
+    {
+        id: "1",
+        name: "Bobby Joe",
+        bio: "The man on the run",
+    },
+    {
+        id: "2",
+        name: "Jane Foster",
+        bio: "Im foster than you",
+    },
+    
+]
+
+server.post('/api/users ', (req, res) => {
+    const user = req.body;
+
+    users.push(user);
+
+    if(!users) {
+        res.status(400).json(`{errorMessage: "Please provide name and bio for the user."}`);
+    } else {
+        res.statusMessage(500).json(`{errorMessage: "There was an error while saving the user to the database."}`);
+    }
+        
+    res.status(201).json(users);
+})
+
+server.get('/api/users', (req, res) => {
+    if(!users) {
+        res.statusMessage(500).json(`{errorMessage: "The users information could not be retrieved."}`)
+    } else {
+        res.status(200).json(users)
+    }
+});
+
+server.get('/api/users/:id', (req, res) => {
+    if(!user) {
+        res.status(404).json(`{errorMessage: the user with the specified Id does not exist.}`);
+    } else {
+        res.status(500).json(`{errorMessage: the user information could not be retrieved.}`);
+    }
+    
+    res.status(200).json(users.id); 
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    users = users.filter(u => u.id !== Number(id));
+
+    if(!user) {
+        res.status(404).json(`{errorMessage: the user with the specified Id does not exist.}`);
+    } else {
+        res.status(500).json(`{errorMessage: the user could not be removed.}`);
+    }
+
+    res.status(200).json(users);
+})
+
+server.patch('/api/users/:id', (req, res) => {
+
+})
+
+const port = 5000;
+
+server.listen(port, () => console.log(`\n == API on port ${port} == \n`));
